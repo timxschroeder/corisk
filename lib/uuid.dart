61a8bac791788c7431132be2dec949467001fa09
Tim Schröder <timxschroeder@gmail.com>
2020-03-21 11:37:13 +0100

    add location model

---
+++ lib/uuid.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+
+///
+/// Source: Flutter source code; copied as the dart uuid package did not support/use strings at the time.
+///
+/// This will generate unique IDs in the format:
+///
+///     f47ac10b-58cc-4372-a567-0e02b2c3d479
+///
+/// ### Example
+///
+///     final String id = Uuid().generateV4();
+class Uuid {
+  final Random _random = Random();
+
+  /// Generate a version 4 (random) uuid. This is a uuid scheme that only uses
+  /// random numbers as the source of the generated uuid.
+  String generateV4() {
+    // Generate xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx / 8-4-4-4-12.
+    final int special = 8 + _random.nextInt(4);
+
+    return '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}-'
+        '${_bitsDigits(16, 4)}-'
+        '4${_bitsDigits(12, 3)}-'
+        '${_printDigits(special, 1)}${_bitsDigits(12, 3)}-'
+        '${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}${_bitsDigits(16, 4)}';
+  }
+
+  String generateRandomNumber(int length) {
+    final StringBuffer randomNumber = StringBuffer();
+    for (var i = 0; i < length; i++) {
+      randomNumber.write(_random.nextInt(9).toString());
+    }
+    return randomNumber.toString();
+  }
+
+  String _bitsDigits(int bitCount, int digitCount) => _printDigits(_generateBits(bitCount), digitCount);
+
+  int _generateBits(int bitCount) => _random.nextInt(1 << bitCount);
+
+  String _printDigits(int value, int count) => value.toRadixString(16).padLeft(count, '0');
+}
